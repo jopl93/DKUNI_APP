0d6a475cdb4f38dda3e282dbabe9e32f3eb93f75
Josephine Plass-Nielsen <jplas@sdu.dk>
2021-04-25 16:19:15 +0200

    Changed the design/colors. Added plotly graph. Now time to see on mobile.

--- public/javascripts/dropdown-functionality.js
+++ public/javascripts/dropdown-functionality.js
@@ -9,4 +9,36 @@ $(document).ready(function(){
         }
     }
     );
+
+
+    $("#choiceOfGraphs").change( function(){
+        console.log(this.value);
+        if(this.value=="OptagKA"){
+            // $('#myDiv').show();
+            d3.csv("./images/SDU_optag_kandidater.csv", function(err, rows){
+        
+                function unpack(rows, key){
+                    return rows.map(function(row) {return row [key]; });
+                }
+        
+            var trace1 = {
+                x: unpack(rows,"Aargang"),
+                y: unpack(rows,"Antal"),
+                type: 'scatter',
+                line: {color: '#a48c74'}
+            }
+        
+            var data = [trace1];
+        
+            Plotly.newPlot('myDiv', data);
+            })
+        } else {
+            // $('#graph').attr("src","");
+            $('#myDiv').empty();
+        }
+    }
+    );
+
+    
 })
+
